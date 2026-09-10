6250778aa89226148d7a06ca657335284f5e5cf9
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-10 09:52:34 +0300

    scaffolded HootDetails and added protected route with hootId param

---
+++ src/components/HootDetails/HootDetails.jsx
@@ -0,0 +1,12 @@
+// src/components/HootDetails/HootDetails.jsx
+
+import { useParams } from 'react-router';
+
+const HootDetails = () => {
+  const { hootId } = useParams();
+  console.log('hootId', hootId);
+
+  return <main>Hoot Details</main>;
+};
+
+export default HootDetails;
