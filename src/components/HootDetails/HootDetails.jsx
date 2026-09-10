2fbc378ead6a8d600fb5b1c8c7c4f9c20fc639e9
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-10 09:54:20 +0300

    created hootService show function and fetched single hoot into state

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,10 +1,21 @@
 // src/components/HootDetails/HootDetails.jsx
 
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+
+import * as hootService from '../../services/hootService';
 
 const HootDetails = () => {
   const { hootId } = useParams();
-  console.log('hootId', hootId);
+  const [hoot, setHoot] = useState(null);
+
+  useEffect(() => {
+    const fetchHoot = async () => {
+      const hootData = await hootService.show(hootId);
+      setHoot(hootData);
+    };
+    fetchHoot();
+  }, [hootId]);
 
   return <main>Hoot Details</main>;
 };
