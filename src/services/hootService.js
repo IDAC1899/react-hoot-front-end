0515e6b7ec1abb910b040fdfa43c8c52ebb63990
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-10 09:46:18 +0300

    created hootService with index function

---
+++ src/services/hootService.js
@@ -0,0 +1,18 @@
+// src/services/hootService.js
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {
+  index,
+};
