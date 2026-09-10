8753ec923db214cbcca092d17112181fa9a0d1de
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-10 09:55:39 +0300

    rendered hoot details and comments section

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -17,7 +17,40 @@ const HootDetails = () => {
     fetchHoot();
   }, [hootId]);
 
-  return <main>Hoot Details</main>;
+  if (!hoot) return <main>Loading...</main>;
+
+  return (
+    <main>
+      <section>
+        <header>
+          <p>{hoot.category.toUpperCase()}</p>
+          <h1>{hoot.title}</h1>
+          <p>
+            {`${hoot.author.username} posted on
+            ${new Date(hoot.createdAt).toLocaleDateString()}`}
+          </p>
+        </header>
+        <p>{hoot.text}</p>
+      </section>
+      <section>
+        <h2>Comments</h2>
+
+        {!hoot.comments.length && <p>There are no comments.</p>}
+
+        {hoot.comments.map((comment) => (
+          <article key={comment._id}>
+            <header>
+              <p>
+                {`${comment.author.username} posted on
+                ${new Date(comment.createdAt).toLocaleDateString()}`}
+              </p>
+            </header>
+            <p>{comment.text}</p>
+          </article>
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default HootDetails;
